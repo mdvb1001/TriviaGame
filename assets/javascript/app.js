41c22d5d3665bd8061ea7e67ef498a56d931134e
maxvanbel <Max@Maximes-MacBook-Pro.local>
2016-11-04 17:19:53 -0500

    add for loop and questions cleanup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,48 +11,49 @@ var game = {
         a: ".5",
         possible: ["1", "1.5", "9", ".5"]
     }, {
-        q: "How many F1 World Championships does Michael Schumacher have between them?",
+        q: "How many F1 Grand Prix did Michael Schumacher win in his career?",
         a: "91",
         possible: ["71", "81", "91", "90"]
-    // }, {
-    //     q: "Who was the French Formula One champion known as 'The Professor'?"
-    //     a: "Alaine Prost",
-    //     possible: [] 
-    // }, {
-    //     q: "I was the Formula 1 World Champion three times and would probably have won more Championships if I wasn't killed in a crash at Imola while leading the race. I was especially talented in the rain. Who am I?"
-    //     a: "Alaine Prost",
-    //     possible: []
-    // }, {
-    //     q: "I was the first German World Champion and I liked it, because I repeated it for 6 more times. What is my name?"
-    //     a: "Michael Schumacher"
-    //     possible: ["Mika Häkkinen", "Jochen Rindt", "Michael Schumacher", "Keke Rosberg"]
-    // }, {
-    //     q: "In what year did 11 different drivers win at least a Grand Prix, but no one won more than 2."
-    //     a: "1982",
-    //     possible: []
-    // }, {
-    //     q: "Politics were still rife in 1982, and the San Marino Grand Prix saw how many cars turn up to compete?"
-    //     a: "14"
-    //     possible: []    
-    // }, {
-    //     q: "How many laps did the 1984 Monaco Grand Prix run for, before it was stopped due to torrential rain?", 
-    //     a: "31"
-    //     possible: 
-    // }, {
-    //     q: "Which driver collapsed, after famously pushing his stricken car over the line at the 1984 Dallas Grand Prix?"
-    //     a: "Nigel Mansell"
-    //     possible: 
-    // }, {
-    //     q: "At which track did Alain Prost record his 28th Grand Prix victory, beating Jackie Stewart's record that had stood since 1973?"
-    //     a: "Estoril"
-    //     possible: []
-    // }, {
-    //     q: "From which grid position, did Nigel Mansell win the 1989 Hungarian Grand Prix?"
-    //     a: "12"
-    //     possible: 
-    // }, {
-    //     q: "Why was Nelson Piquet's 1983 championship win special?"
-    //     a: "First Trubo Champion"
+    }, {
+        q: "Who was the French Formula One champion known as The Professor?",
+        a: "Alain Prost",
+        possible: ["Jean Alesi", "Alain Prost", "Olivier Panis", "Didier Pironi"] 
+    }, {
+        q: "I was the Formula 1 World Champion three times and would probably have won more Championships if I wasn't killed in a crash at Imola while leading the race. I was especially talented in the rain. Who am I?",
+        a: "Ayrton Senna",
+        possible: ["Ayrton Senna", "Gilles Villeneuve", "Nigel Mansell", "Ralph Schumacher"]
+    }, {
+        q: "I was the first German World Champion and I liked it, because I repeated it for 6 more times. What is my name?",
+        a: "Michael Schumacher",
+        possible: ["Mika Häkkinen", "Jochen Rindt", "Michael Schumacher", "Keke Rosberg"]
+    }, {
+        q: "In what year did 11 different drivers win at least a Grand Prix, but no one won more than 2.",
+        a: "1982",
+        possible: ["1980", "1981", "1982", "1984"]
+    }, {
+        q: "Politics were still rife in 1982, and the San Marino Grand Prix saw how many cars turn up to compete?",
+        a: "14",
+        possible: ["26", "21", "18", "14"]    
+    }, {
+        q: "How many laps did the 1984 Monaco Grand Prix run for, before it was stopped due to torrential rain?", 
+        a: "31",
+        possible: ["12", "52", "42", "31"]
+    }, {
+        q: "Which driver collapsed, after famously pushing his stricken car over the line at the 1984 Dallas Grand Prix?",
+        a: "Nigel Mansell",
+        possible: ["Nelson Picket", "Niki Lauda", "Nigel Mansell", "Mario Andretti"]
+    }, {
+        q: "At which track did Alain Prost record his 28th Grand Prix victory, beating Jackie Stewart's record that had stood since 1973?",
+        a: "Estoril",
+        possible: ["Estoril", "Zolder", "Nürburgring", "San Marino"]
+    }, {
+        q: "From which grid position, did Nigel Mansell win the 1989 Hungarian Grand Prix?",
+        a: "12",
+        possible:["6", "8", "12", "14"]
+    }, {
+        q: "Why was Nelson Piquet's 1983 championship win special?",
+        a: "First Turbo Champion",
+        possible: ["It was his last", "First Turbo Champion", "First Brasilian Champion", "It was his last"]
     }]
 };
 
@@ -84,7 +85,6 @@ $(document).ready(function() {
     $('#answers1').append("<div><div><input type='radio' name='question1' value='8'>" + " " + two1 + " " + "</div></div>");
     $('#answers1').append("<div><div><input type='radio' name='question1' value='8'>" + " " + three1 + " " + "</div></div>");
     $('#answers1').append("<div><div><input type='radio' name='question1' value='1'>" + " " + four1 + " " + "</div></div>");
-
 
     var one2 = game.questions[1].possible[0];
     var two2 = game.questions[1].possible[1];
@@ -122,6 +122,17 @@ $(document).ready(function() {
     //     }
 
 
+    for (var i = 0; i < game.questions.length; i++) {
+        var trivia = game.questions[i].q;
+        console.log(trivia);
+        $('.betaQuestions').append("<div><h4>" + trivia + "</h4></div>");
+
+        for (var j = 0; j < game.questions[i].possible.length; j++) {
+            var options = game.questions[i].possible[j];
+            console.log(options);
+            $('.betaQuestions').append("<div><input type='radio' name='question3' value='8'>" + " " + options + " " + "</div>")
+        }
+    }
 
 
     // });
@@ -133,10 +144,7 @@ $(document).ready(function() {
 
     });
 
-
-
     // Execute the run function.
-
 
 });
 
@@ -146,9 +154,9 @@ function result() {
             wins++;
         } else if ($(this).val() === '8') {
             loses++
-        } 
+        }
         notAnswered = (game.questions.length - wins) - loses;
-        
+
     });
     console.log('these are my wins:' + ' ' + wins);
     console.log('these are my loses:' + ' ' + loses);
